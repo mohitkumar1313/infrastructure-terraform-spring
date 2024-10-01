8d33a38f6335d37e7dc6d28570db44d374208423
Mohit Kumar <162154589+mohitkumar1313@users.noreply.github.com>
2024-10-01 12:42:07 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA47CRXWUGW3T5NDG5"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "ca-central-1"
 }
