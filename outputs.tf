b961e5797b36f4d48c7d82ad8b0fc244586d25dd
Mohit Kumar <162154589+mohitkumar1313@users.noreply.github.com>
2024-10-02 12:34:09 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
+output "instance_id" {
+  value = aws_instance.app_server.id
+}
+
 output "instance_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value = aws_instance.app_server.public_ip
 }
